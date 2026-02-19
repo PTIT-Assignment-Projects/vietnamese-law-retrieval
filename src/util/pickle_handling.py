cc19d751f45ef980ac716223c8ed7602e06324da
Duong Vu Cong Tuan <DuongVCT.B22KH024@stu.ptit.edu.vn>
2026-02-19 16:17:05 +0700

    fix pickle handling

--- src/util/pickle_handling.py
+++ src/util/pickle_handling.py
@@ -1,12 +1,22 @@
 import pickle
 from pathlib import Path
 
+from libxslt import cleanup
 
-def save_to_pickle_file(data, file_path: Path) -> None:
-    file_path.parent.mkdir(parents=True, exist_ok=True)
-    with open(file_path, "wb") as file:
-        pickle.dump(data, file, protocol=pickle.HIGHEST_PROTOCOL)
+
+def save_to_pickle_file(file_path: str, data) -> None:
+    path = Path(file_path)
+    path.parent.mkdir(parents=True, exist_ok=True)
+    with path.open("wb") as file:
+        pickle.dump(data, file, protocol=4)
 
 def load_pickle_file(file_path: Path):
     with open(file_path, "rb") as file:
-        return pickle.load(file)
+        contents = file.read()
+        try:
+            pickle.loads(contents)
+            return pickle.load(file)
+        except pickle.UnpicklingError as e:
+            # Handle unpickle error
+            cleanup()
+            exit(1)
