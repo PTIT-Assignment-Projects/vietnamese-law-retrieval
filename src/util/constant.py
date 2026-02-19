5ba68359314a0f3b0d2e0a41118bfc23d25359d3
Duong Vu Cong Tuan <DuongVCT.B22KH024@stu.ptit.edu.vn>
2026-02-19 12:08:57 +0700

    implement preprocessing

---
+++ src/util/constant.py
@@ -0,0 +1,9 @@
+# DATA FILE
+CORPUS_PATH = "data/corpus.csv"
+TRAIN_PATH = "data/train.csv"
+TEST_PATH = "data/test.csv"
+
+
+
+# UTIL FILE
+VIETNAMESE_STOPWORDS_FILE_PATH = "util_file/vietnamese-stopwords.txt"
